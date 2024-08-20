bdbc851fd5b5f4a3b51bb4162ee12ce43c354b5b
Bankole Emmanuel <mrbarnk1@gmail.com>
2024-08-20 03:35:13 +0100

    added ipay in

---
+++ src/vault/app/payin.ts
@@ -0,0 +1,42 @@
+import { ApiClient } from "..";
+import { IPayIn } from "../interfaces/payin";
+
+
+
+// Exchange Container
+export class PayIn extends ApiClient {
+
+    async getFiatRates(): Promise<IPayIn['getFiatRate']> {
+        return this.sendRequest('GET', '/v3/payin/fiat-rates');
+    }
+    async addCard(data: {
+        cardNumber: number;
+        cardHolder: string;
+        cardExpirationYear: number;
+        cardExpirationMonth: number;
+        billingAddress: {
+            zip: string;
+            city: string;
+            state: string;
+            address: string;
+            countryCode: string;
+        }
+    }): Promise<IPayIn['addCard']> {
+        return this.sendRequest('POST', '/v3/payin/card', data);
+    }
+    async createOffer(data: {
+        cardId: number;
+        fromCurrency: string;
+        amount: number;
+        toCurrency: string;
+        operation: 'PAYIN'
+    }): Promise<IPayIn['createOffer']> {
+        return this.sendRequest('POST', '/v3/payin/offer', data);
+    }
+    async updateOffer(data: { offerId: number; currencyFrom: string; currencyTo: string; amountFrom: number; amountTo: number }): Promise<IPayIn['updateOffer']> {
+        return this.sendRequest('PUT', `/v3/payin/pay/${data.offerId}`, data);
+    }
+    async activateOffer(data: { offerId: number, cardCvv?: string, successUrl?: string, fingerprint?: string }): Promise<IPayIn['executeOffer']> {
+        return this.sendRequest('POST', `/payin/pay/${data.offerId}`, data);
+    }
+}
---
+++ src/vault/interfaces/payin.d.ts
@@ -0,0 +1,94 @@
+export interface IPayIn {
+    getFiatRate: {
+        baseCurrency: string;
+        symbolCurrency: string;
+        rate: number;
+    }[]
+    addCard: {
+        cardNumber: number;
+        cardHolder: string;
+        cardExpirationYear: number;
+        cardExpirationMonth: number;
+        billingAddress: {
+            zip: string;
+            city: string;
+            state: string;
+            address: string;
+            countryCode: string;
+        }
+    }
+    createOffer: {
+        offerId?: number; // Defaults to 0
+        expirationTime: string;
+        validSeconds?: number; // Defaults to 0
+        fromCurrency: string;
+        rate: {
+            rate: number; // Defaults to 0
+            currency: string;
+            // };
+            commissionFix?: number; // Defaults to 0
+            commissionPercentage?: number; // Defaults to 0
+            minCrypto?: number; // Defaults to 0
+            maxCrypto?: number; // Defaults to 0
+        }
+        feeInfo: {
+            name: string;
+            value?: number; // Defaults to 0
+            valueOld?: number; // Defaults to 0
+            scale?: number; // Defaults to 0
+            currency: string;
+        }[];
+        limit: {
+            value?: number; // Defaults to 0
+            currency: string;
+        };
+        fees: {
+            currency: string;
+            scale?: number; // Defaults to 0
+            rate?: number; // Defaults to 0
+            partnerFee?: number; // Defaults to 0
+            crypteriumGas?: number; // Defaults to 0
+            additionalFee?: number; // Defaults to 0
+            transactionFee?: number; // Defaults to 0
+            insuranceFee?: number; // Defaults to 0
+            feeTableEnabled?: boolean; // Defaults to true
+            feeTable: {
+                amountFrom: {
+                    value: number; // Defaults to 0
+                    currency: string;
+                };
+                amountTo: {
+                    value: number; // Defaults to 0
+                    currency: string;
+                };
+                percent?: number; // Defaults to 0
+            }[];
+        }
+    }
+    updateOffer: {
+        offerId: number;
+        fromCurrency: string;
+        toCurrency: string;
+        operation: string;
+        toAmount: number;
+        operation: 'PAYIN'
+    }
+    executeOffer: {
+        successUrl: string;
+        failUrl: string;
+        cancelUrl: string;
+        authLink: string;
+        offerId?: number; // Defaults to 0
+        transactionStatus: string;
+        transactionStatusCode?: number; // Defaults to 0
+        originalTransactionStatus: string;
+        originalTransactionStatusCode?: number; // Defaults to 0
+        paymentMode: string;
+        maskedPan: string;
+        transactionDate: string;
+        transactionAmount: string;
+        cardCountryCode: string;
+        cardCountryName: string;
+        cardBankName: string;
+    }
+}
